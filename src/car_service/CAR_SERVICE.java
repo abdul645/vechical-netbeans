651f6da89bf2106483ad79aab0df44c141c71f87
Abdul Azeem <Abdulazeem3802@gmail.com>
2023-06-05 13:58:49 +0530

    CAR Servie

---
+++ src/car_service/CAR_SERVICE.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package car_service;
+import java.util.regex.Pattern;
+/**
+ *
+ * @author Bilal
+ */
+public class CAR_SERVICE {
+//    public static boolean isValid(String email){
+//        String emailFormat= "^[a-zA-Z0-9_+&*-]+(?:\\."+
+//                            ""
+//        
+//    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
